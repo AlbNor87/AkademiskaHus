2438c75def187e72f9bd1aa59ac0815114b063f8
AlbNor87 <albert.norberg@gmail.com>
2018-05-29 01:02:57 +0200

    Added some more awesome stuff

--- public/sw.js
+++ public/sw.js
@@ -1,48 +1,46 @@
-'use strict';
-const VERSION = 'v1';
-var urls = [
-  '/',
-//   '/help',
-//   '/errorreport',
-//   '/malfunction',
-//   '/ideas',
-  '/fonts/SceneAH-Light.otf',
-  '/fonts/SceneAH-Medium.otf',
-  '/fonts/SceneProLight.woff',
-  '/fonts/SceneProMedium.woff',
-  '/images/logo.svg',
-  '/images/bulbBlue.svg',
-  '/images/bulbWhite.svg',
-  '/images/houseGreen.svg',
-  '/images/houseWhite.svg',
-  '/images/trianglePink.svg',
-  '/images/triangleWhite.svg',
-  '/images/location.svg',
-  '/css/app.css',
-  '/js/app.js'
-
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(caches.open(VERSION).then(cache => cache.addAll(urls)));
-});
-  
-// Then we need to active the service worker and/or delete older versions.
-self.addEventListener('activate', event => {
+self.addEventListener('install', function (event) {
+    console.log('SW Installed');
     event.waitUntil(
-        caches.keys().then(keys => {
-        return Promise.all(keys
-            .filter(key => key !== VERSION)
-            .map(key => caches.delete(key))
-        );
+      caches.open('static')
+        .then(function (cache) {
+          cache.addAll([
+            '/',
+            '/help',
+            '/errorReport',
+            '/malfunction',
+            '/ideas',
+            '/fonts/SceneAH-Light.otf',
+            '/fonts/SceneAH-Medium.otf',
+            '/fonts/SceneProLight.woff',
+            '/fonts/SceneProMedium.woff',
+            '/images/logo.svg',
+            '/images/bulbBlue.svg',
+            '/images/bulbWhite.svg',
+            '/images/houseGreen.svg',
+            '/images/houseWhite.svg',
+            '/images/trianglePink.svg',
+            '/images/triangleWhite.svg',
+            '/images/location.svg',
+            '/css/app.css',
+            '/js/app.js'
+          ]);
         })
     );
-});
-
-// Finally we fetch the assets from the service worker or from the network if
-// the browser haven't yet fetched the items.
-self.addEventListener('fetch', event => {
+  });
+  
+  self.addEventListener('activate', function () {
+    console.log('SW Activated');
+  });
+  
+  self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
+      caches.match(event.request)
+        .then(function(res) {
+          if (res) {
+            return res;
+          } else {
+            return fetch(event.request);
+          }
+        })
     );
-});
+  });
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,8 +7,21 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <script>window.Laravel = { csrfToken: '{{ csrf_token() }}' }</script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+        <link rel="manifest" href="/manifest.json">
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#4fbf45">
+
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta name="apple-mobile-web-app-title" content="A-Hus">
+
         <title>Akademiska Hus</title>
     </head>
     <body>
@@ -38,5 +51,14 @@
                     });
                   }
         </script> --}}
+
+        <script>
+            if ('serviceWorker' in navigator) {
+                navigator.serviceWorker.register('/sw.js')
+                .then(function() {
+                console.log('SW registered');
+                });
+            }
+        </script>
     </body>
 </html>
